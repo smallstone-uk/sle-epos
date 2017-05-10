3e64838c22920bbeebb4d8d67481142fd2469b51
James Kingsley <jameslkingsley@outlook.com>
2017-05-10 21:20:09 +0100

    Add new payments bootstrap

---
+++ ePayments/Bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ePayments;
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Ingenico\Connect\Sdk\Client;
+use Ingenico\Connect\Sdk\Communicator;
+use Ingenico\Connect\Sdk\CommunicatorConfiguration;
+use Ingenico\Connect\Sdk\DefaultConnection;
+
+class Bootstrap
+{
+    protected $client;
+    protected $communicator;
+
+    /**
+     * Constructor method.
+     *
+     * @return any
+     */
+    public function __construct()
+    {
+        // Load environment config
+        $dotenv = new Dotenv\Dotenv(__DIR__);
+        $dotenv->load();
+
+        // Setup the SDK
+        $this->setupSdk();
+    }
+
+    /**
+     * Sets up the SDK.
+     *
+     * @return any
+     */
+    public function setupSdk()
+    {
+        $communicatorConfiguration = new CommunicatorConfiguration(
+            getenv('EPAY_APIKEYID'),
+            getenv('EPAY_APISECRET'),
+            getenv('EPAY_BASEURI'),
+            getenv('EPAY_INTEGRATOR')
+        );
+
+        $connection = new DefaultConnection();
+        $this->communicator = new Communicator($connection, $communicatorConfiguration);
+        $this->client = new Client($communicator);
+
+        return $this;
+    }
+}
---
+++ ePayments/Payment.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ePayments;
+
+class Payment extends Bootstrap
+{
+    //
+}
